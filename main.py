d3b0e0c5df0ad20d07e85e43580514d8217c4d95
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2024-01-06 12:26:01 +0530

    implement ml_obj_detection.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from ml_obj_detection import detect
 
 app = FastAPI()
 
@@ -10,7 +9,8 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    out_response = detect(file.file)
 
+    return {"objects": out_response}
